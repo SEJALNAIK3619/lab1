c8558512cc4616c597d77ff5fdae5927eabe93a0
Sejalnaik3619 <168398209+SEJALNAIK3619@users.noreply.github.com>
2024-05-20 08:36:25 +0530

    Add files via upload

---
+++ error.js
@@ -0,0 +1,21 @@
+
+        console.log("JavaScript");
+        setTimeout(()=>{
+            console.log("Hello My Name is Raj....")
+            
+        },5000);
+        try{
+            console.log(data);
+
+        }
+        catch{
+
+            console.log("This is a My Error");
+        }
+        setTimeout(()=>{
+            console.log("Hello My Name is Parth...")
+            
+        },7500);setTimeout(()=>{
+            console.log("Hello My Name is Jay...")
+            
+        },10000);
