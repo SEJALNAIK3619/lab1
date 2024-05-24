43f33caf7fccb35813d9d9600bcf4b548e65af4e
Sejalnaik3619 <168398209+SEJALNAIK3619@users.noreply.github.com>
2024-05-24 08:29:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Homefile from './Homefile';
+import Footer from './Footer';
+import About from './About';
+import Usestate from './Usestate'
+function App() {
+  
+return (
+<>
+<h3 align="center"></h3>
+    <h2 align="center">React Js</h2>
+    <p>
+    hi hello how are you
+    hi hello how are you
+    hi hello how are you
+    hi hello how are you
+      hi hello how are you
+    </p>
+     <Header/>
+      <Homefile />
+     <Footer />
+      <About />
+      <Usestate/>
+  </>
+    );}
+export default App;
